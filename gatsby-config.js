176abda0c082855fae53301dc6b556cab4e3cb08
Shaun Axani <shaunaxani@Shauns-iMac-9.local>
2019-12-13 20:49:52 -0800

    have md figured out

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `markdown-pages`,
+        name: `blog`,
         path: `${__dirname}/blog`,
       },
     },
